d852a0c5f5215a781ba9ef59ad1012c46256534c
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2024-08-25 13:42:58 +0530

    Refactored into a main file

--- neural_network.py
+++ neural_network.py
@@ -1,8 +1,6 @@
 from abc import abstractmethod
 import numpy as np
-from numpy.random.mtrand import sample
 import data
-from data.datasets import spiral_data, vertical_data
 
 data.init()
 
@@ -89,39 +87,3 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         self.dinputs = dvalues.copy()
         self.dinputs[range(samples), y_true] -= 1
         self.dinputs = self.dinputs / samples
-
-
-# X, y = vertical_data(samples=100, classes=3)
-X, y = spiral_data(samples=100, classes=3)
-
-dense1 = Layer_Dense(2, 3)
-activation1 = Activation_ReLU()
-dense2 = Layer_Dense(3, 3)
-
-loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
-
-dense1.forward(X)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-
-loss = loss_activation.forward(dense2.output, y)
-print(loss_activation.output[:5])
-
-print('loss:', loss)
-
-predictions = np.argmax(loss_activation.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions==y)
-
-print('acc:', accuracy)
-
-loss_activation.backward(loss_activation.output, y)
-dense2.backward(loss_activation.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward(activation1.dinputs)
-
-print(dense1.dweights)
-print(dense1.dbiases)
-print(dense2.dweights)
-print(dense2.dbiases)
