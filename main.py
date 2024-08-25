cb2bfe3026a9de1d5dfa61848c2fd0030701ea0e
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2024-08-25 21:36:27 +0530

    Implemented learning rate for sdg

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ dense1 = Layer_Dense(2, 64)
 activation1 = Activation_ReLU()
 dense2 = Layer_Dense(64, 3)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
-optimizer = Optimizer_SGD()
+optimizer = Optimizer_SGD(decay=1e-3)
 
 for epoch in range(10001):
     dense1.forward(X)
@@ -25,12 +25,15 @@ for epoch in range(10001):
     if not epoch % 100:
         print(f'epoch: {epoch}, ' +
             f'acc: {accuracy:.3f}, ' +
-            f'loss: {loss:.3f}')
+            f'loss: {loss:.3f}, ' +
+            f'lr: {optimizer.current_learning_rate}')
 
     loss_activation.backward(loss_activation.output, y)
     dense2.backward(loss_activation.dinputs)
     activation1.backward(dense2.dinputs)
     dense1.backward(activation1.dinputs)
 
+    optimizer.pre_update_params()
     optimizer.update_params(dense1)
     optimizer.update_params(dense2)
+    optimizer.post_update_params()
