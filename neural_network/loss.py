4106b5e0448219316978779335e85313a1db639f
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2024-09-01 03:03:00 +0530

    Completed the loss file

--- neural_network/loss.py
+++ neural_network/loss.py
@@ -6,23 +6,36 @@ class Loss:
         regularization_loss = 0
         for layer in self.trainable_layers:
             if layer.weight_regularizer_l1 > 0:
-                regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+                regularization_loss += layer.weight_regularizer_l1 *                                       np.sum(np.abs(layer.weights))
             if layer.weight_regularizer_l2 > 0:
-                regularization_loss += layer.weight_regularizer_l2 * np.sum(layer.weights * layer.weights)
+                regularization_loss += layer.weight_regularizer_l2 *                                       np.sum(layer.weights *                                              layer.weights)
             if layer.bias_regularizer_l1 > 0:
-                regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+                regularization_loss += layer.bias_regularizer_l1 *                                       np.sum(np.abs(layer.biases))
             if layer.bias_regularizer_l2 > 0:
-                regularization_loss += layer.bias_regularizer_l2 * np.sum(layer.biases * layer.biases)
+                regularization_loss += layer.bias_regularizer_l2 *                                       np.sum(layer.biases *                                              layer.biases)
         return regularization_loss
 
     def remember_trainable_layers(self, trainable_layers):
         self.trainable_layers = trainable_layers
 
     def calculate(self, output, y, *, include_regularization=False):
-        data_loss = np.mean(self.forward(output, y))
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        self.accumulated_sum += np.sum(sample_losses)
+        self.accumulated_count += len(sample_losses)
         if not include_regularization:
             return data_loss
         return data_loss, self.regularization_loss()
+
+    def calculate_accumulated(self, *, include_regularization=False):
+        data_loss = self.accumulated_sum / self.accumulated_count
+        if not include_regularization:
+            return data_loss
+        return data_loss, self.regularization_loss()
+
+    def new_pass(self):
+        self.accumulated_sum = 0
+        self.accumulated_count = 0
 
     @abstractmethod
     def forward(self, y_pred, y_true):
@@ -32,14 +45,12 @@ class Loss_CategoricalCrossentropy(Loss):
     def forward(self, y_pred, y_true):
         samples = len(y_pred)
         y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
-
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[range(samples), y_true]
         elif len(y_true.shape) == 2:
             correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
         else:
-            raise ValueError('shape of y_true is wrong')
-
+            raise ValueError("Shape of y_true is wrong")
         return -np.log(correct_confidences)
 
     def backward(self, dvalues, y_true):
@@ -66,7 +77,8 @@ class Loss_BinaryCrossentropy(Loss):
 
 class Loss_MeanSquaredError(Loss):
     def forward(self, y_pred, y_true):
-        return np.mean((y_true - y_pred) ** 2, axis=-1)
+        sample_losses = np.mean((y_true - y_pred)**2, axis=-1)
+        return sample_losses
 
     def backward(self, dvalues, y_true):
         samples = len(dvalues)
@@ -76,7 +88,8 @@ class Loss_MeanSquaredError(Loss):
 
 class Loss_MeanAbsoluteError(Loss):
     def forward(self, y_pred, y_true):
-        return np.mean(np.abs(y_true - y_pred), axis=-1)
+        sample_losses = np.mean(np.abs(y_true - y_pred), axis=-1)
+        return sample_losses
 
     def backward(self, dvalues, y_true):
         samples = len(dvalues)
