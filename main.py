2b48776729e0863d1bcbaedc641ad13a46621db9
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2024-09-01 02:20:19 +0530

    Refactored code into separate files

--- main.py
+++ main.py
@@ -1,75 +1,28 @@
-import numpy as np
-import data
-from data.datasets import spiral_data, vertical_data, sine_data
-from neural_network import Activation_Linear, Activation_Sigmoid, Layer_Dense, Activation_ReLU, Activation_Softmax_Loss_CategoricalCrossentropy, Layer_Dropout, Loss_BinaryCrossentropy, Loss_MeanSquaredError, Optimizer_Adagrad, Optimizer_Adam, Optimizer_RMSprop, Optimizer_SGD
+from data.datasets import sine_data
+from neural_network import Model
+from neural_network.accuracy import Accuracy_Regression
+from neural_network.activation import Activation_Linear, Activation_ReLU
+from neural_network.layer import Layer_Dense
+from neural_network.loss import Loss_MeanSquaredError
+from neural_network.optimizer import Optimizer_Adam
 
-# X, y = vertical_data(samples=100, classes=3)
-# X, y = spiral_data(samples=100, classes=2)
+model = Model()
+
 X, y = sine_data()
 
-dense1 = Layer_Dense(1, 64)
-activation1 = Activation_ReLU()
-dense2 = Layer_Dense(64, 64)
-activation2 = Activation_ReLU()
-dense3 = Layer_Dense(64, 1)
-activation3 = Activation_Linear()
-loss_function = Loss_MeanSquaredError()
+model.add(Layer_Dense(1, 64))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(64, 64))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(64, 1))
+model.add(Activation_Linear())
 
-optimizer = Optimizer_Adam(learning_rate=0.005, decay=1e-3)
+model.set(
+    loss=Loss_MeanSquaredError(),
+    optimizer=Optimizer_Adam(learning_rate=0.005, decay=1e-3),
+    accuracy=Accuracy_Regression()
+)
 
-accuracy_precision = np.std(y) / 250
+model.finalize()
 
-for epoch in range(10001):
-    dense1.forward(X)
-    activation1.forward(dense1.output)
-    dense2.forward(activation1.output)
-    activation2.forward(dense2.output)
-    dense3.forward(activation2.output)
-    activation3.forward(dense3.output)
-
-    data_loss = loss_function.calculate(activation3.output, y)
-
-    regularization_loss = loss_function.regularization_loss(dense1) + loss_function.regularization_loss(dense2) + loss_function.regularization_loss(dense3)
-
-    loss = data_loss + regularization_loss
-
-    predictions = activation3.output
-    accuracy = np.mean(np.absolute(predictions - y) < accuracy_precision)
-
-    if not epoch % 100:
-        print(f'epoch: {epoch}, ' +
-              f'acc: {accuracy:.3f}, ' +
-              f'loss: {loss:.3f} (' +
-              f'data_loss: {data_loss:.3f}, ' +
-              f'reg_loss: {regularization_loss:.3f}), ' +
-              f'lr: {optimizer.current_learning_rate}')
-
-    loss_function.backward(activation3.output, y)
-    activation3.backward(loss_function.dinputs)
-    dense3.backward(activation3.dinputs)
-    activation2.backward(dense3.dinputs)
-    dense2.backward(activation2.dinputs)
-    activation1.backward(dense2.dinputs)
-    dense1.backward(activation1.dinputs)
-
-    optimizer.pre_update_params()
-    optimizer.update_params(dense1)
-    optimizer.update_params(dense2)
-    optimizer.update_params(dense3)
-    optimizer.post_update_params()
-
-
-import matplotlib.pyplot as plt
-
-X_test, y_test = sine_data()
-
-dense1.forward(X_test)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
-dense3.forward(activation2.output)
-activation3.forward(dense3.output)
-
-plt.plot(X_test, y_test)
-plt.plot(X_test, activation3.output)
-plt.show()
+model.train(X, y, epochs=10000, print_every=100)
---
+++ neural_network/__init__.py
@@ -0,0 +1,6 @@
+from .model import Model
+from .layer import *
+from .optimizer import *
+from .activation import *
+from .loss import *
+from .accuracy import *
